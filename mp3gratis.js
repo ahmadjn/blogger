6972f27fa4f11e9a6a431b415d4bfaa6d9c5d62f
ahmadjn <59970179+ahmadjn@users.noreply.github.com>
2020-11-15 01:23:10 +0800

    update 2

--- mp3gratis.js
+++ mp3gratis.js
@@ -2,5 +2,10 @@ var myArray = ['https://suratku.net/', 'https://how-to.buzz', 'https://dlfreeima
 var rand = Math.floor(Math.random() * myArray.length);
 
 function showquote(){
-    document.getElementById('link').innerHTML="<a href='"+myArray[rand]+"' target='_blank' title='Download Gratis MP3'><img src='https://1.bp.blogspot.com/-aliILkEQaj0/X7AOhfI0o5I/AAAAAAAACOg/MoKhZUyejFg-GH2k19dYxgJ51MnukSZIQCLcBGAsYHQ/s336-rw/download-mp3-gratis.jpg' style='width:100%;' alt='MP3 Gratis' title='Download MP3 Gratis'><center><h3>Download MP3 Gratis</h3></center></a>";}
+    document.getElementById('link').innerHTML="
+    <a href='"+myArray[rand]+"' target='_blank' title='Download Gratis MP3'>
+    <img src='https://1.bp.blogspot.com/-aliILkEQaj0/X7AOhfI0o5I/AAAAAAAACOg/MoKhZUyejFg-GH2k19dYxgJ51MnukSZIQCLcBGAsYHQ/s336-rw/download-mp3-gratis.jpg' style='width:100%;' alt='MP3 Gratis' title='Download MP3 Gratis'>
+    <h3 style='margin:0px; text-align: center;'>Download MP3 Gratis</h3>
+    </a>
+    ";}
 showquote();
