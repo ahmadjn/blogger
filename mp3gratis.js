2346ae9567a8910cb1f7caa98d114ffafcf851e0
ahmadjn <59970179+ahmadjn@users.noreply.github.com>
2020-11-16 13:31:00 +0800

    up up

--- mp3gratis.js
+++ mp3gratis.js
@@ -4,8 +4,10 @@ var rand = Math.floor(Math.random() * myArray.length);
 function showquote(){
     document.getElementById('link').innerHTML="
     <a href='"+myArray[rand]+"' target='_blank' title='Download Gratis MP3'>
-    <img src='https://1.bp.blogspot.com/-aliILkEQaj0/X7AOhfI0o5I/AAAAAAAACOg/MoKhZUyejFg-GH2k19dYxgJ51MnukSZIQCLcBGAsYHQ/s336-rw/download-mp3-gratis.jpg' style='width:100%;' alt='MP3 Gratis' title='Download MP3 Gratis'>
-    <h3 style='margin:0px; padding: 10px; text-align: center; background-color:Tomato; color:White;'>Download MP3 Gratis</h3>
+        <img src='https://1.bp.blogspot.com/-aliILkEQaj0/X7AOhfI0o5I/AAAAAAAACOg/MoKhZUyejFg-GH2k19dYxgJ51MnukSZIQCLcBGAsYHQ/s336-rw/download-mp3-gratis.jpg' style='width:100%;' alt='MP3 Gratis' title='Download MP3 Gratis'>
+    </a>
+    <a href='"+myArray[rand]+"' target='_blank' title='Download MP3 Gratis'>
+        <h3 style='margin:0px; padding: 10px; text-align: center; background-color:Tomato; color:White;'>Download MP3 Gratis</h3>
     </a>
     ";}
 showquote();
